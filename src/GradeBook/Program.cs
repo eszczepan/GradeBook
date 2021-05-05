cea52659fd63a0ffce9843800d0dc4e6b1c886fe
Szczepan <s.szablej1@gmail.com>
2021-05-05 18:08:07 +0200

    Calculate grades and print it out

--- src/GradeBook/Program.cs
+++ src/GradeBook/Program.cs
@@ -6,20 +6,39 @@ namespace GradeBook
   {
     static void Main(string[] args)
     {
-      var x = 34.2;
-      var y = 65.5;
+      double[] grades = new double[] { 4, 3, 5, 2, 2.5, 3.5, 5, 2, 2, 3.5, 4.5 };
+      double worstGrade = 6;
+      double bestGrade = 0;
+      double sum = 0;
 
-      var result = x + y;
-
-      if (args.Length > 0 && x > 30)
+      if (grades.Length > 0)
       {
-        Console.WriteLine($"Hello {args[0]}!");
-        Console.WriteLine(result);
+        for (int i = 0; i < grades.Length; i++)
+        {
+          var currGrade = grades[i];
+
+          if (currGrade < worstGrade)
+          {
+            worstGrade = currGrade;
+          }
+
+          if (currGrade > bestGrade)
+          {
+            bestGrade = currGrade;
+          }
+
+          sum += currGrade;
+        }
       }
       else
       {
-        Console.WriteLine("Something went wrong :(");
+        Console.WriteLine("You have 0 grades yet.");
       }
+
+
+      Console.WriteLine($"Hello {args[0]}! that is your worst grade: {worstGrade}");
+      Console.WriteLine($"Here's your best: {bestGrade}");
+      Console.WriteLine($"And your average grade: {sum / grades.Length}");
     }
   }
 }
