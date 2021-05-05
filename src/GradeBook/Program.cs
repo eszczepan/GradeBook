f81ad7dc4476b1a602c29ed87d11276e9002ebcb
Szczepan <s.szablej1@gmail.com>
2021-05-05 18:32:02 +0200

    Improve check statements

--- src/GradeBook/Program.cs
+++ src/GradeBook/Program.cs
@@ -6,12 +6,13 @@ namespace GradeBook
   {
     static void Main(string[] args)
     {
-      double[] grades = new double[] { 4, 3, 5, 2, 2.5, 3.5, 5, 2, 2, 3.5, 4.5 };
+      var grades = new double[] { 4, 3, 5, 2, 2.5, 3.5, 5, 2, 2, 3.5, 4.5 };
       double worstGrade = 6;
       double bestGrade = 0;
       double sum = 0;
+      System.Console.WriteLine();
 
-      if (grades.Length > 0)
+      if (grades.Length > 0 && args.Length > 0)
       {
         for (int i = 0; i < grades.Length; i++)
         {
@@ -29,16 +30,19 @@ namespace GradeBook
 
           sum += currGrade;
         }
+        Console.WriteLine($"Hello {args[0]}!");
+        Console.WriteLine($"This is your worst grade: {worstGrade}");
+        Console.WriteLine($"Here's your best: {bestGrade}");
+        Console.WriteLine($"And your average grade: {sum / grades.Length}");
+      }
+      else if (args.Length == 0)
+      {
+        Console.WriteLine("Type your name in.");
       }
       else
       {
         Console.WriteLine("You have 0 grades yet.");
       }
-
-
-      Console.WriteLine($"Hello {args[0]}! that is your worst grade: {worstGrade}");
-      Console.WriteLine($"Here's your best: {bestGrade}");
-      Console.WriteLine($"And your average grade: {sum / grades.Length}");
     }
   }
 }
