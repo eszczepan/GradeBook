59bd5d1b71c05b4e9b5f438cea7eb6d2bc99d315
Szczepan <s.szablej1@gmail.com>
2021-05-05 17:31:37 +0200

    Check if syntax working properly

--- src/GradeBook/Program.cs
+++ src/GradeBook/Program.cs
@@ -6,7 +6,20 @@ namespace GradeBook
   {
     static void Main(string[] args)
     {
-      Console.WriteLine("Hello Szczepan!");
+      var x = 34.2;
+      var y = 65.5;
+
+      var result = x + y;
+
+      if (args.Length > 0 && x > 30)
+      {
+        Console.WriteLine($"Hello {args[0]}!");
+        Console.WriteLine(result);
+      }
+      else
+      {
+        Console.WriteLine("Something went wrong :(");
+      }
     }
   }
 }
