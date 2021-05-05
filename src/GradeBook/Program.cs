486197c4567c6981bc48f820a4b521cc5dffdae7
Szczepan <s.szablej1@gmail.com>
2021-05-05 18:58:28 +0200

    Change array to List and add average grade formatting

--- src/GradeBook/Program.cs
+++ src/GradeBook/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace GradeBook
 {
@@ -6,34 +7,31 @@ namespace GradeBook
   {
     static void Main(string[] args)
     {
-      var grades = new double[] { 4, 3, 5, 2, 2.5, 3.5, 5, 2, 2, 3.5, 4.5 };
+      var grades = new List<double>() { 4.0, 3.0, 5.0, 2.0, 4.5, 3.5, 5.0, 2.0, 2.0, 3.5, 4.5 };
       double worstGrade = 6;
       double bestGrade = 0;
-      double sum = 0;
-      System.Console.WriteLine();
+      double sum = 0.0;
 
-      if (grades.Length > 0 && args.Length > 0)
+      if (grades.Count > 0 && args.Length > 0)
       {
-        for (int i = 0; i < grades.Length; i++)
+        foreach (var grade in grades)
         {
-          var currGrade = grades[i];
-
-          if (currGrade < worstGrade)
+          if (grade < worstGrade)
           {
-            worstGrade = currGrade;
+            worstGrade = grade;
           }
 
-          if (currGrade > bestGrade)
+          if (grade > bestGrade)
           {
-            bestGrade = currGrade;
+            bestGrade = grade;
           }
 
-          sum += currGrade;
+          sum += grade;
         }
         Console.WriteLine($"Hello {args[0]}!");
         Console.WriteLine($"This is your worst grade: {worstGrade}");
         Console.WriteLine($"Here's your best: {bestGrade}");
-        Console.WriteLine($"And your average grade: {sum / grades.Length}");
+        Console.WriteLine($"And your average grade is: {(sum / grades.Count):N2}");
       }
       else if (args.Length == 0)
       {
