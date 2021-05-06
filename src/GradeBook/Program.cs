6a171d854fd4e238d8ce4b5fdf50972559fd3773
Szczepan <s.szablej1@gmail.com>
2021-05-06 12:54:14 +0200

    Write tests for reference types

--- src/GradeBook/Program.cs
+++ src/GradeBook/Program.cs
@@ -11,7 +11,7 @@ namespace GradeBook
       book.AddGrade(3.0);
       book.AddGrade(4.0);
       var stats = book.GetStats();
-      // Console.WriteLine($"Hello {this.name}!");
+      Console.WriteLine($"Hello {book.Name}!");
       Console.WriteLine($"This is your worst grade: {stats.Low:N1}");
       Console.WriteLine($"Here's your best: {stats.High:N1}");
       Console.WriteLine($"And your average grade is: {stats.Average:N2}");
